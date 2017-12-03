035d0b1fd4679e9f1c6960aca42ebbcddf929b13
Bahaa Eldin <bhaaface@gmail.com>
2017-12-03 05:11:00 +0200

    GUI->List,Remove,AddNew

---
+++ src/twitter/api/TwitterAPI.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitter.api;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author BahaMoviX
+ */
+public class TwitterAPI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        main sh = new main();
+        sh.show();
+    }
+    
+}
